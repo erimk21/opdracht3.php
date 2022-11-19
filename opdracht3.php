96ab9046dbd8467700afa2869b4b2d3dcb7040fe
erimk21 <9019259@student.zadkine.nl>
2022-11-19 23:39:56 +0100

    opzet 3

---
+++ opdracht3.php
@@ -0,0 +1,17 @@
+<?php
+
+$tekst1 = "<h1>zaterdag</h1>";
+$tekst2 = "<h1>midden weekend</h1>";
+
+echo "$tekst1";
+echo "$tekst2";
+
+
+$getal1 = 5;
+$getal2 = 15;
+
+print "$getal1</br>";
+print "$getal2";
+
+
+?>
